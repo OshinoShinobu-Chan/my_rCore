a1271cf1a60a8c8c893cbefec18b8f5ac353cf17
root <root@Crimmy>
2024-02-13 14:50:14 +0800

    ch7

--- user/src/bin/initproc.rs
+++ user/src/bin/initproc.rs
@@ -1,16 +1,14 @@
 #![no_std]
 #![no_main]
 
-#[macro_use]
 extern crate user_lib;
 
 use user_lib::{exec, fork, wait, yield_};
 
 #[no_mangle]
 fn main() -> i32 {
-    let mut shell_pid = fork();
-    if shell_pid == 0 {
-        exec("user_shell\0");
+    if fork() == 0 {
+        exec("user_shell\0", &[core::ptr::null::<u8>()]);
     } else {
         loop {
             let mut exit_code: i32 = 0;
@@ -18,17 +16,14 @@ fn main() -> i32 {
             if pid == -1 {
                 yield_();
                 continue;
-            } else if pid == shell_pid {
-                println!("[initproc] User shell exited with code {}", exit_code);
-                println!("[initproc] Start another user shell...");
-                shell_pid = fork();
-                if shell_pid == 0 {
-                    exec("user_shell\0");
-                }
-            } else {
-                println!("[initproc] Release a zombie process, pid={}, exit_code={}",
-                    pid, exit_code);
             }
+            /*
+            println!(
+                "[initproc] Released a zombie process, pid={}, exit_code={}",
+                pid,
+                exit_code,
+            );
+            */
         }
     }
     0
--- user/src/syscall.rs
+++ user/src/syscall.rs
@@ -1,12 +1,19 @@
 use core::arch::asm;
+use crate::SignalAction;
 
+const SYSCALL_DUP: usize = 24;
 const SYSCALL_OPEN: usize = 56;
 const SYSCALL_CLOSE: usize = 57;
+const SYSCALL_PIPE: usize = 59;
 const SYSCALL_READ: usize = 63;
 const SYSCALL_WRITE: usize = 64;
 const SYSCALL_EXIT: usize = 93;
 const SYSCALL_YIELD: usize = 124;
-const SYSCALL_SHUTDOWN: usize = 129;
+const SYSCALL_KILL: usize = 129;
+const SYSCALL_SHUTDOWN: usize = 130;
+const SYSCALL_SIGACTION: usize = 134;
+const SYSCALL_SIGPROCMASK: usize = 135;
+const SYSCALL_SIGRETURN: usize = 139;
 const SYSCALL_GET_TIME: usize = 169;
 const SYSCALL_GETPID: usize = 172;
 const SYSCALL_FORK: usize = 220;
@@ -27,12 +34,20 @@ fn syscall(id: usize, args: [usize; 3]) -> isize {
     ret
 }
 
+pub fn sys_dup(fd: usize) -> isize {
+    syscall(SYSCALL_DUP, [fd, 0, 0])
+}
+
 pub fn sys_open(path: &str, flags: u32) -> isize {
     syscall(SYSCALL_OPEN, [path.as_ptr() as usize, flags as usize, 0])
 }
 
 pub fn sys_close(fd: usize) -> isize {
     syscall(SYSCALL_CLOSE, [fd, 0, 0])
+}
+
+pub fn sys_pipe(pipe_fd: &mut [usize; 2]) -> isize {
+    syscall(SYSCALL_PIPE, [pipe_fd.as_mut_ptr() as usize, 0, 0])
 }
 
 pub fn sys_read(fd: usize, buffer: &mut [u8]) -> isize {
@@ -52,10 +67,37 @@ pub fn sys_yield() -> isize {
     syscall(SYSCALL_YIELD, [0, 0, 0])
 }
 
+// system call used for sending signal to other processes
+pub fn sys_kill(pid: usize, signum: i32) -> isize {
+    syscall(SYSCALL_KILL, [pid, signum as usize, 0])
+}
+
+// system call for set the action when signal is received
+pub fn sys_sigaction(
+    signum: i32,
+    action: *const SignalAction,
+    old_action: *mut SignalAction,
+) -> isize {
+    syscall(
+        SYSCALL_SIGACTION, 
+        [signum as usize, action as usize, old_action as usize]
+    )
+}
+
 // system call for shutdown machine
 pub fn sys_shutdown(failure: usize) -> ! {
     syscall(SYSCALL_SHUTDOWN, [failure, 0, 0]);
     unreachable!();
+}
+
+// system call used for mask/unmask signals
+pub fn sys_sigprocmask(mask: u32) -> isize {
+    syscall(SYSCALL_SIGPROCMASK, [mask as usize, 0, 0])
+}
+
+// system call used for returning from signal handler
+pub fn sys_sigreturn() -> isize {
+    syscall(SYSCALL_SIGRETURN, [0, 0, 0])
 }
 
 // system call used for getting time in milliseconds
@@ -74,8 +116,8 @@ pub fn sys_fork() -> isize {
 }
 
 // system call used for exec a new program
-pub fn sys_exec(path: &str) -> isize {
-    syscall(SYSCALL_EXEC, [path.as_ptr() as usize, 0, 0])
+pub fn sys_exec(path: &str, args: &[*const u8]) -> isize {
+    syscall(SYSCALL_EXEC, [path.as_ptr() as usize, args.as_ptr() as usize, 0])
 }
 
 // system call used for wait child process
